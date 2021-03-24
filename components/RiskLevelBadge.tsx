21a88b2d632c5fc78ad8130d38e433afd1feb731
polymatter <jlake.jobs@gmail.com>
2021-03-24 20:29:14 +0000

    use fragment to return multiple tags

--- components/RiskLevelBadge.tsx
+++ components/RiskLevelBadge.tsx
@@ -9,40 +9,49 @@ const HighRed = Colors.badgeLevels.High;
 const MediumYellow = Colors.badgeLevels.Medium;
 const LowGreen = Colors.badgeLevels.Low;
 
-const RiskLevelBadge = ({level}:{level: RiskLevel}) => {
-    switch(level) {
-        case 'High':
-            return <Text style={[styles.levelBadge, styles.levelBadgeHigh]}><MaterialIcons name="dangerous" size={24} color={HighRed} />High</Text>
-        case 'Medium':
-            return <Text style={[styles.levelBadge, styles.levelBadgeMedium]}><MaterialIcons name="warning" size={24} color={MediumYellow} />Medium</Text>
-        case 'Low':
-            return <Text style={[styles.levelBadge, styles.levelBadgeLow]}><MaterialCommunityIcons name="emoticon-happy-outline" size={24} color={LowGreen} />Low</Text>
-    }
+const RiskLevelBadge = ({ level }: { level: RiskLevel }) => {
+  switch (level) {
+    case 'High':
+      return <>
+        <MaterialIcons name="dangerous" size={24} color={HighRed} />
+        <Text style={[styles.levelBadge, styles.levelBadgeHigh]}>High</Text>
+      </>
+    case 'Medium':
+      return <>
+        <MaterialIcons name="warning" size={24} color={MediumYellow} />
+        <Text style={[styles.levelBadge, styles.levelBadgeMedium]}>Medium</Text>
+      </>
+    case 'Low':
+      return <>
+        <MaterialCommunityIcons name="emoticon-happy-outline" size={24} color={LowGreen} />
+        <Text style={[styles.levelBadge, styles.levelBadgeLow]}>Low</Text>
+      </>
+  }
 }
 
 const styles = StyleSheet.create({
-    label: {
-        display: 'flex',
-        flexGrow: 10
-    },
-    levelBadge: {
-        display: 'flex',
-        flexShrink: 0,
-        borderRadius: 15,
-        fontSize: 12,
-        paddingLeft: 8,
-        paddingRight: 8,
-        marginRight: 30,
-    },
-    levelBadgeLow: {
-        color: LowGreen,
-    },
-    levelBadgeMedium: {
-        color: MediumYellow,
-    },
-    levelBadgeHigh: {
-        color: HighRed,
-    },
+  label: {
+    display: 'flex',
+    flexGrow: 10
+  },
+  levelBadge: {
+    display: 'flex',
+    flexShrink: 0,
+    borderRadius: 15,
+    fontSize: 12,
+    paddingLeft: 8,
+    paddingRight: 8,
+    marginRight: 30,
+  },
+  levelBadgeLow: {
+    color: LowGreen,
+  },
+  levelBadgeMedium: {
+    color: MediumYellow,
+  },
+  levelBadgeHigh: {
+    color: HighRed,
+  },
 });
 
 export default RiskLevelBadge
