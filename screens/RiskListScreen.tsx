9066a352a3600436b838c6bb21643495e34514fb
polymatter <jlake.jobs@gmail.com>
2021-03-24 19:48:39 +0000

    minor formatting

--- screens/RiskListScreen.tsx
+++ screens/RiskListScreen.tsx
@@ -1,16 +1,15 @@
 import * as React from 'react';
-import { Button, StyleSheet, ScrollView, Pressable } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
-import RiskSummary from '../components/RiskSummary';
-import { Text, View } from '../components/Themed';
-import { BottomTabParamList, RiskDetail, RiskListTabParamList } from '../types';
+import { StyleSheet, ScrollView, Pressable } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 
-export default function RiskListScreen({navigation} : {navigation: StackNavigationProp<RiskListTabParamList, 'RiskListScreen'>}) {
+import { RiskDetail, RiskListTabParamList } from '../types';
+import { View } from '../components/Themed';
+import RiskSummary from '../components/RiskSummary';
 
-  const buttonClick = (risk:RiskDetail) => {
-    navigation.navigate('RiskDetailScreen', {risk});
+export default function RiskListScreen({ navigation }: { navigation: StackNavigationProp<RiskListTabParamList, 'RiskListScreen'> }) {
+
+  const buttonClick = (risk: RiskDetail) => {
+    navigation.navigate('RiskDetailScreen', { risk });
   }
 
   const [risks, setRisks] = React.useState([] as RiskDetail[]);
@@ -22,12 +21,10 @@ export default function RiskListScreen({navigation} : {navigation: StackNavigati
     <View style={styles.container}>
       <ScrollView>
         {risks.map(risk => {
-          return <Pressable onPress={() => buttonClick(risk)}><RiskSummary key={risk.id} risk={risk}></RiskSummary></Pressable>
+          return <Pressable key={risk.id} onPress={() => buttonClick(risk)}><RiskSummary key={risk.id} risk={risk}></RiskSummary></Pressable>
         })
         }
       </ScrollView>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {/* <EditScreenInfo path="/screens/RiskListScreen.tsx" /> */}
     </View>
   );
 }
@@ -37,14 +34,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
   },
 });
