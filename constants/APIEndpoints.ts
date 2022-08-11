1c272d23ee58907003b7d09b50586d2f5b6b6193
polymatter <jlake.jobs@gmail.com>
2022-08-11 14:49:16 +0100

    for error checking later

--- constants/APIEndpoints.ts
+++ constants/APIEndpoints.ts
@@ -1,23 +1,21 @@
 export default {
-    riskDetailList: 'https://411uchidwl.execute-api.eu-west-2.amazonaws.com/dev/risks',
+  riskDetailList: 'https://411uchidwl.execute-api.eu-west-2.amazonaws.com/dev/risks',
 }
 
 const updateRiskLabelParams = {
-    url: 'https://411uchidwl.execute-api.eu-west-2.amazonaws.com/dev/risks',
-    fetchOptions: {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+  url: 'https://411uchidwl.execute-api.eu-west-2.amazonaws.com/dev/risks',
+  fetchOptions: {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
     }
+  }
 }
 
-export function updateRiskLabel(payload: { id: string, label: string }): void {
-    const options = {
-      ...updateRiskLabelParams.fetchOptions,
-      body: JSON.stringify(payload)
-    };
-    fetch(updateRiskLabelParams.url, options)
-      .then(console.log)
-      .catch(console.error);
+export function updateRiskLabel(payload: { id: string, label: string }): Promise<Response> {
+  const options = {
+    ...updateRiskLabelParams.fetchOptions,
+    body: JSON.stringify(payload)
+  };
+  return fetch(updateRiskLabelParams.url, options);
 }
