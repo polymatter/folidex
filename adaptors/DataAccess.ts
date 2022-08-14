72cd37bd5812453e3976c7d555c2f73ae12f4ffc
Kevin Lake <it@audax.uk>
2022-08-14 18:25:50 +0100

    fix race condition on reading environment variables

--- adaptors/DataAccess.ts
+++ adaptors/DataAccess.ts
@@ -1,18 +1,18 @@
 import { API_URL } from '@env'
 
-const updateRiskLabelParams = {
-  url: API_URL,
-  fetchOptions: {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }
-}
-
 export type updateRiskLabelPayload = { id: string, label: string }
 
 export function updateRiskLabel(payload: updateRiskLabelPayload, options: Omit<RequestInit, 'body' | 'method' | 'headers'> = {}): Promise<Response> {
+  const updateRiskLabelParams = {
+    url: API_URL,
+    fetchOptions: {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  }
+
   const fetchOptions: RequestInit = {
     ...options,
     ...updateRiskLabelParams.fetchOptions,
@@ -21,16 +21,15 @@ export function updateRiskLabel(payload: updateRiskLabelPayload, options: Omit<R
   return fetch(updateRiskLabelParams.url, fetchOptions);
 }
 
-const getRiskDetailListParams = {
-  url: API_URL,
-  fetchOptions: {
-    headers: {
-      'Content-Type': 'application/json'
+export function getRiskDetailList(options: RequestInit = {}): Promise<Response> {
+  const getRiskDetailListParams = {
+    url: API_URL,
+    fetchOptions: {
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
   }
-}
-
-export function getRiskDetailList(options: RequestInit = {}): Promise<Response> {
   const fetchOptions = {
     ...options,
     ...getRiskDetailListParams.fetchOptions
