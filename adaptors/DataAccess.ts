28d5fff96f9e26e0c856b5b82802ee7e09cbc41e
polymatter <jlake.jobs@gmail.com>
2022-08-15 12:25:43 +0100

    bugfix environment variables not read

--- adaptors/DataAccess.ts
+++ adaptors/DataAccess.ts
@@ -1,39 +1,25 @@
 import { API_URL } from '@env'
 
-const updateRiskLabelParams = {
-  url: API_URL,
-  fetchOptions: {
+export type updateRiskLabelPayload = { id: string, label: string }
+
+export function updateRiskLabel(payload: updateRiskLabelPayload, options: Omit<RequestInit, 'body' | 'method'> = {}): Promise<Response> {
+  const fetchOptions: RequestInit = {
+    ...options,
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-    }
-  }
-}
-
-export type updateRiskLabelPayload = { id: string, label: string }
-
-export function updateRiskLabel(payload: updateRiskLabelPayload, options: Omit<RequestInit, 'body' | 'method' | 'headers'> = {}): Promise<Response> {
-  const fetchOptions: RequestInit = {
-    ...options,
-    ...updateRiskLabelParams.fetchOptions,
+    },
     body: JSON.stringify(payload)
   };
-  return fetch(updateRiskLabelParams.url, fetchOptions);
+  return fetch(API_URL, fetchOptions);
 }
 
-const getRiskDetailListParams = {
-  url: API_URL,
-  fetchOptions: {
+export function getRiskDetailList(options: Omit<RequestInit, 'body' | 'method'> = {}): Promise<Response> {
+  const fetchOptions = {
+    ...options,
     headers: {
       'Content-Type': 'application/json'
     }
-  }
-}
-
-export function getRiskDetailList(options: RequestInit = {}): Promise<Response> {
-  const fetchOptions = {
-    ...options,
-    ...getRiskDetailListParams.fetchOptions
   };
-  return fetch(getRiskDetailListParams.url, fetchOptions);
+  return fetch(API_URL, fetchOptions);
 }
