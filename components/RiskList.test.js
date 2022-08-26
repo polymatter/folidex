3b9d14f5cffa1a86da4c49a90a4349a518b0ca54
polymatter <jlake.jobs@gmail.com>
2022-08-26 16:40:17 +0100

    (test) test selecting a risk item

--- components/RiskList.test.js
+++ components/RiskList.test.js
@@ -1,20 +1,23 @@
 import * as React from 'react';
-import renderer from 'react-test-renderer';
+import { render, screen, fireEvent } from '@testing-library/react-native';
 import RiskStoreContext from '../store/RiskStore';
 import mockdata from '../mocks/RiskStore.json';
 import RiskList from './RiskList';
 
 describe('<RiskList>', () => {
 
-  test('can render a risk list', () => {
+  test('can render a risk list and be able to select a risk item', () => {
 
-    const mockNavigation = jest.mock();
-    const tree = renderer.create(
+    const mockNavigation = jest.fn();
+    const tree = render(
       <RiskStoreContext.Provider value={mockdata}>
         <RiskList onPressRiskSummary={mockNavigation} showIcon={false} />
       </RiskStoreContext.Provider>
     ).toJSON();
 
     expect(tree).toMatchSnapshot();
+
+    fireEvent.press(screen.getByText(mockdata[2].label));
+    expect(mockNavigation).toBeCalled();
   })
 })
